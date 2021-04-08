c4db5cbf6e11b044936d010d959849a91aecfabe
TheEnderOfficial <70067741+TheEnderOfficial@users.noreply.github.com>
2021-04-08 17:15:51 +0300

    Arduino Code
    Add all commands and handlers to Serial

--- arduinocode/arduinocode.ino
+++ arduinocode/arduinocode.ino
@@ -9,15 +9,63 @@
 #define STAPSK  "Robo.Grade"
 #endif
 
+#define u8 uint8_t
+
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
 ESP8266WebServer server(80);
 
-String handleCmd(DynamicJsonDocument doc){
-  const char* command = doc["cmd"];
-  Serial.println(command);
-  auto dataS = "{\"success\": 1}";
+String handleCmd(DynamicJsonDocument doc) {
+  const char* _command = doc["cmd"];
+  // Что-бы получить command: (String) _command
+  auto dataS = "{\"success\": 0}";
+  if ((String) _command == "fan") {
+    /*
+      {
+       "cmd": "fan",
+       "data": {
+         "fan_number": 32,
+         "action": false
+       }
+      }
+    */
+    const u8 fan_number = doc["data"]["fan_number"];
+    const u8 action = (u8) doc["data"]["action"];
+    Serial.println("Fan command requested: fun_number: [" + String(fan_number) + "], action: [" + String(action) + "]");
+    dataS = "{\"success\": 1}";
+  }
+  else if ((String) _command == "light") {
+    /*
+      {
+       "cmd": "light",
+       "data": {
+         "pixel_number": 0,
+         "action" : 1
+       }
+      }
+    */
+    const u8 pixel_number = doc["data"]["pixel_number"];
+    const u8 action = doc["data"]["action"];
+
+    Serial.println("Light command requested: pixel_number: [" +  String(pixel_number) + "], action: [" + String(action) + "]");
+    dataS = "{\"success\": 1}";
+  }
+  else if ((String) _command == "sensor") {
+    /*
+      {
+       "cmd": "sensor",
+       "data": {
+         "name": "dht"
+       }
+      }
+    */
+    // Что-бы получить name: (String) _name
+    const char* _name = doc["data"]["name"];
+  
+    Serial.println("Sensor command requested: name: [" +  String(_name) + "]");
+    dataS = "{\"success\": 1, \"sensorData1\": 0, \"sensorData2\": 0, \"sensorData3\": 0, \"sensorDataX\": 0}";
+  }
   return dataS;
 }
 void handleRoot() {
